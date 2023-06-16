cf2b1629c69d02f5394b27fcf50bd0f454606163
Rudr <rudrprasad@yahoo.com>
2023-06-16 13:18:17 +1200

    wehhh

--- .eleventy.js
+++ .eleventy.js
@@ -6,11 +6,10 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy("./src/app.js");
     eleventyConfig.addPassthroughCopy("./src/assets");
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-
-        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-
-    })
+    eleventyConfig.addFilter("justYear", (dateString) => {
+        dateObj = new Date(dateString);
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy');
+    });
 
 
     return{
